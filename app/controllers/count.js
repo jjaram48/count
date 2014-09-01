634d1e1fe97386fe2aa57f5a7d54cce392dece6d
Julian Jaramillo <jjaram48@gmail.com>
2014-09-01 08:29:39 -0500

    Organized merge of commits

--- app/controllers/count.js
+++ app/controllers/count.js
@@ -1,20 +1,5 @@
 import Ember from 'ember';
 
-// <<<<<<< HEAD
-// var countController = {
-//   totalCount : 0,
-//   nullCount: true,
-//   actions: {
-
-//     addCount: function () {
-//       this.set('totalCount', this.totalCount.get() + 1);
-//       this.nullCount.set(false);
-//     }
-//   }
-// };
-
-// export default Ember.Controller.extend({
-// =======
 export default Ember.Controller.extend({
 
   countValue: 0,
@@ -35,5 +20,4 @@ export default Ember.Controller.extend({
 
   },
 
-// >>>>>>> c71376137b354230f3d789910417fbcf1930c371
 });
